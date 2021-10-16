ff52c1adc549e2915eedb50b6a2da2d985d52185
xXH3XYXx <h3xybaby@gmail.com>
2021-10-16 00:23:26 -0500

    main

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const discord = require('discord.js');
-const client = new discord.Client({
+const discord = require('discord.js'); // Importing Discord API
+const PREFIX = process.env.PREFIX; // Getting Prefix From .env File.
+const client = new discord.Client({ // Creating new DisCord Client 
     intents: [
         discord.Intents.FLAGS.GUILDS,
-        discord.Intents.FLAGS.GUILD_MESSAGES
+        discord.Intents.FLAGS.GUILD_MESSAGES  // Discord.js v13 Require to list bot intents
     ]
 });
-const dotenv = require('dotenv');
+const dotenv = require('dotenv'); // Importing Dotenv So we can use .env Files 
 dotenv.config();
 
 
 client.on('ready', () => {
-    console.log(`${client.user.tag} Has Logged in`);
+    console.log(`${client.user.tag} Has Logged in`);  // logging to console when Boty has started 
 });
 
 
 client.on('messageCreate', (message) => {
-   if (message.content === 'ping') {
-       message.reply({
-           content: 'pong!!',
-       });
-   }
-} )
+    if (message.author.bot) return;
+    if (message.content.toLocaleLowerCase().startsWith(PREFIX + "Hello")) {
+        message.reply("hello");
+    };
+};  // Client message event ping replay with pong Test function
+   
 
 client.login(process.env.TOKEN);
